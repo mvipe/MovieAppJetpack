c58a1c3a9d115f0ad5ce2d4d2eb0cd6d265325de
mvipe <mohnishvats@gmail.com>
2022-06-14 23:29:17 +0530

    added the top bar and back navigation

--- app/src/main/java/com/learn/movieappjetpack/screens/home/DetailsScreen.kt
+++ app/src/main/java/com/learn/movieappjetpack/screens/home/DetailsScreen.kt
@@ -1,32 +1,53 @@
 package com.learn.movieappjetpack.screens.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?){
-   Surface(modifier = Modifier
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
+            Row() {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back",
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                })
+                Spacer(modifier = Modifier.width(100.dp))
+                Text(text = "Movie Details")
+            }
+        }
+    }) {
+           Surface(modifier = Modifier
        .fillMaxWidth()
        .fillMaxHeight()) {
-       Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-           Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+               Column(
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Center
+               ) {
+                   Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
 
-           Spacer(modifier = Modifier.height(23.dp))
-           Button(onClick = {
-               navController.popBackStack()
-           }) {
-               Text(text = "Go Back")
-           }
+                   Spacer(modifier = Modifier.height(23.dp))
+                   Button(onClick = {
+                       navController.popBackStack()
+                   }) {
+                       Text(text = "Go Back")
+                   }
 
-       }
+               }
 
-   }
-}
+           }}}
+    
+    
+    
+    
+
+
