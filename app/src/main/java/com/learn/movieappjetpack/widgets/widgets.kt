24959ccca2c0c1d69dc5a2889ffcf3698e7650fc
mvipe <mohnishvats@gmail.com>
2022-06-16 00:11:43 +0530

    Added expand capability on the card to show more info

--- app/src/main/java/com/learn/movieappjetpack/widgets/widgets.kt
+++ app/src/main/java/com/learn/movieappjetpack/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.learn.movieappjetpack.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,7 +10,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,8 +20,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
@@ -29,11 +37,16 @@ import com.learn.movieappjetpack.models.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie= getMovies()[0], onItemClick:(String)->Unit={}){
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
         .clickable { onItemClick(movie.id) }
-        .height(130.dp),
+      ,
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp) {
         Row(verticalAlignment = Alignment.CenterVertically,
@@ -74,8 +87,40 @@ fun MovieRow(movie: Movie= getMovies()[0], onItemClick:(String)->Unit={}){
                     , style = MaterialTheme.typography.h6)
                 Text(text = "Release: ${movie.title}",color= Color.Black
                     , style = MaterialTheme.typography.h6)
+
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                       Text(buildAnnotatedString {
+                           withStyle(style = SpanStyle(
+                               color = Color.DarkGray,
+                               fontSize = 13.sp
+                           )){
+                               append("Plot: ")
+                           }
+
+                           withStyle(style = SpanStyle(
+                               color = Color.DarkGray,
+                               fontSize = 13.sp,
+                               fontWeight = FontWeight.Bold
+                           )){
+                               append("${movie.plot}")
+                           }
+                       })
+                    }
+                }
+
+                Icon(imageVector = if(expanded) Icons.Filled.KeyboardArrowDown
+                    else Icons.Filled.KeyboardArrowUp, contentDescription = "Down Arrow"
+                    , modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint=Color.DarkGray)
             }
         }
 
     }
+    
+
 }
